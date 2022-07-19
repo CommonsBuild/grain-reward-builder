a03a6ff6772cb6363c7406076175f6c2ebb36391
Mateo Daza <mateodaza@gmail.com>
2022-07-19 17:02:58 -0500

    more commenta

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { fetchAllUsers, getGrainData, manager } from "./utils";
 
 const main = async (): Promise<void> => {
   await connectDB();
-  const allUsers = await fetchAllUsers();
+  const dbUsers = await fetchAllUsers();
 
   await manager.reloadLedger();
   const ledger = manager.ledger;
@@ -12,9 +12,9 @@ const main = async (): Promise<void> => {
 
   console.log({ grainData });
   console.log({
-    // ledger,
-    // allUsers,
-    // grainData,
+    // ledger,   // in _accounts we get the info from sourcecred
+    // dbUsers,  // we have the addresses here from database
+    // grainData, // all grain distributed in here
     myself: ledger._accounts.get("SW3QLzfLRvQkXcmy4MxMNg"),
   });
 };
