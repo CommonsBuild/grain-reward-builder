7d8e33e49ee46274add5329ec9230b243f9df055
0xNuggan <nugganite@outlook.com>
2022-07-25 12:57:41 +0200

    update existing development

---
+++ clean_data.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import re
+import ast
+import csv, json
+
+# This script takes the raw data pulled from sourcecred an cleans it up to make it readable:
+
+# ==== ACTIVATED USER ACCOUNTS ====
+# This table contains the users who took part in the "Sourcecred Mass activation", linking different accounts together
+
+raw_read = pd.read_csv("./raw_data/activatedUsers.csv")
+
+activatedSeriesArray = []
+for index, entry in raw_read.iterrows():
+    activatedSeriesArray.append(pd.Series(ast.literal_eval(entry["_doc"])))
+
+activated_users = pd.DataFrame(activatedSeriesArray)
+
+print(activated_users.head())
+activated_users.to_csv("./clean_data/activatedUsers.csv", index=False)
+
+
+# ==== GRAIN DISTRIBUTIONS ====
+# This table contains the grain distributions exported from the ledger, while discarding all empty distributions
+
+grain = pd.read_csv("./raw_data/grain.csv")
+grain = grain.loc[:, (grain != 0).any(axis=0)]
+print(grain.head())
+grain.to_csv("./clean_data/grainDistributions.csv", index=False)
+
+# ==== ACCOUNTS FROM THE LEDGER ====
+# This table contains the accounts as they are stored in the Sourcecred ledger
+# I've chosen to keep absolutely everything for now ti be sure, but for further use all those Dataframes inside of dataframes will need to be trimmed. To avoid issues, I've replaced the \u0000 caracters in the aliases with backslashes
+
+
+# Convert the CSV file to JSON
+jsonData = []
+with open("./raw_data/ledgerAccounts.csv", encoding="utf-8") as csvf:
+    csvReader = csv.DictReader(csvf)
+
+    # Build each row reading the stored strings as dict
+    # and add it to data
+    for row in csvReader:
+        buildRow = {}
+        for elem in row:
+            try:
+                buf = ast.literal_eval(row[elem])
+            except:
+                # for non activated accounts the export is saved empty instead of 0
+                # we correct this here.
+                buf = "0"
+
+            buildRow[elem] = buf
+        jsonData.append(buildRow)
+
+
+ledgerDF = pd.DataFrame(jsonData)
+
+# Now we have to sanitize the strings stored the "identity" dict. For some reason, these strings are riddled with the null character "\u0000"
+# To make future retrieval easy, we split the strings by this null character into an array
+for index, row in ledgerDF.iterrows():
+    row["identity"]["address"] = row["identity"]["address"].split("\u0000")
+    for alias in row["identity"]["aliases"]:
+        # print(len(row["identity"]["aliases"]))
+        for elem in alias:
+            alias[elem] = alias[elem].split("\u0000")
+            # print(alias[elem])
+
+print(ledgerDF.head())
+ledgerDF.to_csv("./clean_data/ledgerData.csv", index=False)
--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,61 @@ const main = async (): Promise<void> => {
 
   await manager.reloadLedger();
   const ledger = manager.ledger;
+  const ledgerAccounts = ledger._accounts;
 
   const grainData = await getGrainData(ledger);
 
-  console.log({ grainData });
-  console.log({
-    // ledger,   // in _accounts we get the info from sourcecred
-    // dbUsers,  // we have the addresses here from database
-    // grainData, // all grain distributed in here
-    myself: ledger._accounts.get("SW3QLzfLRvQkXcmy4MxMNg"),
-  });
+
+
+
+
+  let jsonLedger = {};  
+  ledgerAccounts.forEach((value, key) => {  
+    jsonLedger[key] = value  
+  });  
+  console.log(jsonLedger)
+  //console.log(typeof jsonLedger)
+
+
+
+  let jsonUsers = {};  
+  dbUsers.forEach((value, key) => {  
+    jsonUsers[key] = value  
+  });  
+  let activeUsers = jsonUsers
+
+
+  // console.log( dbUsers);
+  // // console.log({
+  // //   ledgerAccounts });
+  // console.log({
+  //   // ledger,   // in _accounts we get the info from sourcecred
+  //   // dbUsers,  // we have the addresses here from database
+  //   // grainData, // all grain distributed in here
+  //   myself: ledger._accounts.get("SW3QLzfLRvQkXcmy4MxMNg"),
+  // });
+
+
+
+  const ObjectsToCsv = require('objects-to-csv');
+
+// If you use "await", code must be inside an asynchronous function:
+(async () => {
+
+  const grainCsv = new ObjectsToCsv(grainData);
+  await grainCsv.toDisk('./raw_data/grain.csv');
+
+  const ledgerCsv = new ObjectsToCsv(Object.values(jsonLedger));
+  await ledgerCsv.toDisk('./raw_data/ledgerAccounts.csv')
+
+
+  const activatedCsv = new ObjectsToCsv(Object.values(activeUsers));
+  await activatedCsv.toDisk('./raw_data/activatedUsers.csv')
+
+
+
+
+})();
 };
 
 main();
